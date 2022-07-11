f8472975e23dfb5f264975075e4a335266fede3f
slavko.boldin <slavko.boldin@cloudsense.com>
2022-07-11 13:00:36 +0200

    first commit

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export interface SelectProps {
+  optionLabels: string[];
+}
+
+const Select = (props: SelectProps) => {
+  return (
+    <select>
+      {props.optionLabels.map((opt) => (
+        <option value={opt}>{opt}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
